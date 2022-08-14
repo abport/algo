ae643afbbfe37227114e44abf5c53b262df1e8ce
Amin Beheshti <81535210+abport@users.noreply.github.com>
2022-08-15 00:45:53 +0430

    Update isSubsequence.py

--- Is Subsequence/isSubsequence.py
+++ Is Subsequence/isSubsequence.py
@@ -23,7 +23,7 @@ class Solution(object):
     # Second Method
     i , j = 0, 0
     
-    while i < len(s) and j < len(j):
+    while i < len(s) and j < len(t):
         if s[i] == t[j]:
             i += 1
         j += 1
