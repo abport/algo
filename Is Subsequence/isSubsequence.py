2b9c22cc9b187d91ea52e7db0b35fd3bae95ba24
Amin Beheshti <81535210+abport@users.noreply.github.com>
2022-08-15 00:44:40 +0430

    Update isSubsequence.py

--- Is Subsequence/isSubsequence.py
+++ Is Subsequence/isSubsequence.py
@@ -18,3 +18,13 @@ class Solution(object):
             if i == len(s):
                 break
         return i == len(s)
+    
+    
+    # Second Method
+    i , j = 0, 0
+    
+    while i < len(s) and j < len(j):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return True if i == len(s) else False
